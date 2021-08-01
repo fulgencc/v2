b168a5966b46467b1b39447aca2d09306ef30a73
Chris Fulgencio <fulgencc@uci.edu>
2021-08-01 04:11:37 -0700

    initial site stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Portfolio",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-sass",
+  {
+    resolve: 'gatsby-plugin-eslint',
+    options: {
+      stages: ['develop'],
+      extensions: ['js', 'jsx'],
+      exclude: ['node_modules', '.cache', 'public'],
+      // Any eslint-webpack-plugin options below
+    }
+  }
+  ],
 };
