27822175c6bfe29d575ddc1e1eb45485fd98336a
Chris Fulgencio <fulgencc@uci.edu>
2021-08-04 17:22:59 -0700

    added jobs component wip

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
+import { motion } from 'framer-motion';
+import * as styles from './jobs.module.scss';
 
-const Jobs = () => (
-  <section>
-    <p>hello world</p>
-  </section>
-);
+const Jobs = () => {
+  const [selectedJob, setSelectedJob] = React.useState('Ohana');
+  const [tabHighlightHeight, setTabHighlightHeight] = React.useState(0);
+  const jobList = ['Ohana', 'Public Works', 'Eiswelt'];
+  return (
+    <div className={styles.tab_list} role="tablist">
+      {jobList.map((job, idx) => (
+        <button
+          id={idx}
+          type="button"
+          role="tab"
+          tabIndex={selectedJob === job ? '0' : '-1'}
+          aria-selected={selectedJob === job ? 'true' : 'false'}
+          aria-controls="panel-0"
+          onClick={(e) => {
+            setTabHighlightHeight(((idx + 1) * e.currentTarget.offsetHeight) - 50);
+            console.log(((idx + 1) * e.currentTarget.offsetHeight) - 50);
+            setSelectedJob(job);
+          }}
+          className={styles.tab_button}
+        >
+          {job}
+        </button>
+      ))}
+      <motion.div className={styles.tab_highlight} animate={{ y: tabHighlightHeight }} />
+    </div>
+  );
+};
 
 export default Jobs;
